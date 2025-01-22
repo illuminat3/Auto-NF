2284d3052c1f41cc51b48e1733bb676b616ed56d
MatthewRawson <mrawson@redev.co.uk>
2025-01-22 14:34:21 +0000

    Init Commit

---
+++ src/client/java/xyz/illuminat3/AutoNFClient.java
@@ -0,0 +1,10 @@
+package xyz.illuminat3;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoNFClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
