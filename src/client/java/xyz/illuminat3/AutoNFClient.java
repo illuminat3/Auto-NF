8870fc423549d6f98aa3a91bdfa1e64755c18169
MatthewRawson <mrawson@redev.co.uk>
2025-01-22 15:53:52 +0000

    Added Implementation for fishing events

--- src/client/java/xyz/illuminat3/AutoNFClient.java
+++ src/client/java/xyz/illuminat3/AutoNFClient.java
@@ -1,10 +1,32 @@
 package xyz.illuminat3;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 
 public class AutoNFClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientReceiveMessageEvents.ALLOW_CHAT.register((message, signedMessage, sender, params, receptionTimestamp) -> {
+			String messageContent = message.getString();
+
+			if (messageContent.contains("FISHING ▶")) {
+				if (messageContent.contains("Mythical")) {
+					sendChatMessage("gg");
+				} else if (messageContent.contains("Platinum")) {
+					sendChatMessage("nf");
+				}
+			}
+			return true;
+		});
+	}
+
+	// FISHING ▶ Mythical
+	// FISHING ▶ Platinum
+
+	private void sendChatMessage(String message) {
+		var client = net.minecraft.client.MinecraftClient.getInstance();
+		if (client.player != null) {
+			client.player.networkHandler.sendChatMessage(message);
+		}
 	}
 }
